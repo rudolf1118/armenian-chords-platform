82d7e41ece084aa6f41e980c73a340d48ee26495
rudolf.harutyunyan <rudolf.harutyunyan@pipe17.com>
2025-07-13 03:04:20 +0400

    feat: initialize song service with Prisma, PostgreSQL, and Docker setup

---
+++ song-service/prisma/migrations/20250712215812_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Artist" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Artist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "chords" TEXT NOT NULL,
+    "artistId" INTEGER NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Song" ADD CONSTRAINT "Song_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "Artist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ song-service/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GQLHealth } from './gql.health';
+import { SongModule } from './song/song.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+    SongModule
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService, GQLHealth],
+  exports: [PrismaService]
+})
+export class AppModule {}
